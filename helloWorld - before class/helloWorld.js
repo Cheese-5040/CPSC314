0e206de20b18377b376d1c54209bba9dd63496bd
Cheese <owyongcheeseng@gmail.com>
2022-01-19 10:44:15 -0800

    ass 1

--- helloWorld - before class/helloWorld.js
+++ helloWorld - before class/helloWorld.js
@@ -21,27 +21,28 @@ var scene = new THREE.Scene();
 // SETS: OBJECT(S) IN THE SCENE
 
 // WORLD COORDINATE FRAME: other objects are defined with respect to it
-var worldFrame = new THREE.AxesHelper(5) ;
-scene.add(worldFrame);
+var worldFrame = new THREE.AxesHelper(5) ; //adding the axis, RGB -> XYZ
+scene.add(worldFrame); 
 
 // EARTH
 var earthGeometry = new THREE.SphereGeometry(5, 32, 32);
 
 // version 1: Basic material
-var earthMaterial = new THREE.MeshPhongMaterial( { color: 0xff0000, wireframe: false } );
+//var earthMaterial = new THREE.MeshPhongMaterial( { color: 0xff0000, wireframe: false } );
 
 // version 2: Textured material
-// var earthColorTexture = 
-//     new THREE.TextureLoader().load('images/earthmap1k.jpg');
+var earthColorTexture = 
+    new THREE.TextureLoader().load('images/earthmap1k.jpg');
 
-// var earthBumpTexture = 
-//     new THREE.TextureLoader().load('images/earthbump1k.jpg');
+    //bumpmap means shows the height field of all the bumps in the earth 
+var earthBumpTexture = 
+    new THREE.TextureLoader().load('images/earthbump1k.jpg');
 
-// var earthMaterial = new THREE.MeshPhongMaterial( 
-//     {
-//     	map: earthColorTexture,
-// 	    bumpMap: earthBumpTexture
-//     } );
+var earthMaterial = new THREE.MeshPhongMaterial( 
+    {
+    	map: earthColorTexture,
+	    bumpMap: earthBumpTexture
+    } );
 
 // version 3: Simple Shader Material
 // var earthMaterial = new THREE.ShaderMaterial( {
@@ -59,7 +60,7 @@ var pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(-70,100,-70);
 scene.add(pointLight);    
 
-// CAMERA
+// CAMERA Object
 var camera = new THREE.PerspectiveCamera(30, 1, 0.1, 1000); // view angle, aspect ratio, near, far
 //var camera = new THREE.OrthographicCamera(-10,10,10,-10,1,1000); // l,r,t,b,n,f
 //camera.position.set(10,15,40);
@@ -69,7 +70,7 @@ scene.add(camera);
 
 // SETUP ORBIT CONTROL OF THE CAMERA
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.damping = 0.2;
+controls.damping = 0.2; //for mouse to move around
 
 // ADAPT TO WINDOW RESIZE
 function resize() {
